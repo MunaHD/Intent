276be7cb6a1c46f71c7e76f505f335a50c6033fc
Muna Dahir <muna.dahir@hotmail.com>
2022-02-03 22:32:30 -0500

    setup backend

---
+++ server/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const cors = require("cors");
+
+// this will be how we connect the server to our DB
+// and then we pass the db variable to our route functions
+const { Pool } = require("pg");
+const dbParams = require("./lib/db.js");
+const db = new Pool(dbParams);
+db.connect();
+
+const homeRouter = require("./routes/home");
+
+const app = express();
+
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", homeRouter);
+
+module.exports = app;
+exports.db = db;
