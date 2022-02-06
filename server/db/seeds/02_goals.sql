33a66d3524814bb58f2b3a045864a39dcbb7f06d
Muna Dahir <muna.dahir@hotmail.com>
2022-02-06 00:19:44 -0500

    remove the decription in schema and seeds

--- server/db/seeds/02_goals.sql
+++ server/db/seeds/02_goals.sql
@@ -1,9 +1,12 @@
 
-INSERT INTO goals (name, description, status, isComplete, user_id)
+INSERT INTO goals (name,  status, isComplete, user_id)
 VALUES
-('Muscle up', 'Able to do 3 muscleups in one session',1, false, 1),
-('Pushups', '25 pushups', 0, false, 1),
-('Splits', 'Full splits', 1, false,  2),
-('Handstand','A one minute handstand', 3, false, 2);
+('Muscle up', 20, false, 1),
+('Pushups', 0, false, 1),
+('Splits', 80, false,  1),
+('Handstand', 60, false, 1),
+('Pushups', 0, false, 2),
+('Splits', 80, false,  2),
+('Handstand', 60, false, 2);
 
 
