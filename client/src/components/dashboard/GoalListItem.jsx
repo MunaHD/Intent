e75554c6c410275d3a558e68adabf997f0ecb15d
Muna Dahir <muna.dahir@hotmail.com>
2022-02-06 09:56:05 -0500

    render the goal card with the progress bar as well as the delete and journal buttons

--- client/src/components/dashboard/GoalListItem.jsx
+++ client/src/components/dashboard/GoalListItem.jsx
@@ -4,20 +4,33 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import NotesIcon from "@mui/icons-material/Notes";
+import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 import Circle from "./Circle";
 import "./home.css";
 
 export default function Header(props) {
+  const { id, name, status, iscomplete, deleteGoal, completeGoal } = props;
+
+  const clickHandler = () => {
+    deleteGoal(id);
+  };
   return (
     <Card sx={{ minWidth: 275 }}>
-      <div className='card-content'>
+      <div className='goal-card-content'>
+        <Typography>{name}</Typography>
         <CardContent className='progress-bar'>
-          <Circle getProgress={props.getProgress} />
+          <Circle completeGoal={completeGoal} status={status} id={id} />
         </CardContent>
         <CardActions className='button-group'>
-          <Button size='small'>Delete</Button>
-          <Button size='small'>Journal</Button>
+          <Button size='small' onClick={clickHandler}>
+            <DeleteForeverIcon style={{ fill: "red" }} />
+          </Button>
+          <Button size='small'>
+            <NotesIcon />
+            <Typography></Typography>
+          </Button>
         </CardActions>
       </div>
     </Card>
