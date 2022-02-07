ca4b7dd0ed002d0a8f7e3ca9e903c8ca434a32ae
Muna Dahir <muna.dahir@hotmail.com>
2022-02-07 00:52:40 -0500

    add users_id to schema and seeds

--- server/db/seeds/03_journals.sql
+++ server/db/seeds/03_journals.sql
@@ -1,9 +1,13 @@
 
-INSERT INTO journals (entry, goal_id)
+INSERT INTO journals (entry, goal_id, user_id)
 VALUES
-('Today, was great.', 1),
-('I found it hard to concentrate.', 1),
-('It was fun.', 2),
-('Still trying', 2);
+('Today, was great.', 1, 1),
+('I found it hard to concentrate.', 1, 1),
+('It was fun.', 2, 1),
+('Still trying', 1, 1),
+('Today, was great.', 1, 1),
+('I found it hard to concentrate.', 1, 2),
+('It was fun.', 2, 2),
+('Still trying', 2, 2);
 
 
