5489626a66e850edb4f0040cf5b0cecc264fb096
Muna Dahir <muna.dahir@hotmail.com>
2022-02-07 00:52:04 -0500

    add route for journals

--- server/app.js
+++ server/app.js
@@ -24,10 +24,12 @@ app.use(express.static(path.join(__dirname, "public")));
 const homeRouter = require("./routes/home");
 const loginRouter = require("./routes/login");
 const goalsRouter = require("./routes/goals");
+const journalsRouter = require("./routes/journals");
 
 app.use("/", homeRouter);
 app.use("/login", loginRouter(db));
 app.use("/goals", goalsRouter(db));
+app.use("/journals", journalsRouter(db));
 
 module.exports = app;
 exports.db = db;
