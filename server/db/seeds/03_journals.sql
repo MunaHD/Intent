9d2dd8d3264a99243d8ec4637835f69a146551e8
Muna Dahir <muna.dahir@hotmail.com>
2022-02-07 01:43:16 -0500

    change seed to render different goal categories

--- server/db/seeds/03_journals.sql
+++ server/db/seeds/03_journals.sql
@@ -4,10 +4,10 @@ VALUES
 ('Today, was great.', 1, 1),
 ('I found it hard to concentrate.', 1, 1),
 ('It was fun.', 2, 1),
-('Still trying', 1, 1),
-('Today, was great.', 1, 1),
+('Still trying', 3, 1),
+('Today, was great.', 4, 1),
 ('I found it hard to concentrate.', 1, 2),
-('It was fun.', 2, 2),
-('Still trying', 2, 2);
+('It was fun.', 5, 1),
+('Still trying', 7, 2);
 
 
