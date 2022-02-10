87b6267d8b6cf574e5b25ecb344683f23265dadd
Muna Dahir <muna.dahir@hotmail.com>
2022-02-09 23:21:26 -0500

    remoe speed category from the seeds

--- server/db/seeds/02_goals.sql
+++ server/db/seeds/02_goals.sql
@@ -2,11 +2,11 @@
 INSERT INTO goals (name, status, category, user_id)
 VALUES
 ('Muscle up', 20, 'Strength', 1),
-('Pushups', 0, 'Strength', 1),
+('Pushups', 0, 'Endurance', 1),
 ('Splits', 80, 'Flexibility',  1),
-('Sprint', 60,'Speed', 1),
-('Pushups', 0, 'Strength', 2),
-('Splits', 80, 'Flexibility',  2),
+('Sprint', 60,'Cardio', 1),
+('Pushups', 0, 'Endurance', 1),
+('Splits', 80, 'Strength',  1),
 ('Handstand', 60, 'Strength', 2);
 
 
