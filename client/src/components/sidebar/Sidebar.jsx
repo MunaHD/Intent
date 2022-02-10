4334626604c1c09dd3fdf8ed04f49c5329a061fe
Muna Dahir <muna.dahir@hotmail.com>
2022-02-09 23:19:41 -0500

    remove extra imports and dead code

--- client/src/components/sidebar/Sidebar.jsx
+++ client/src/components/sidebar/Sidebar.jsx
@@ -1,11 +1,7 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
-import CssBaseline from "@mui/material/CssBaseline";
-import AppBar from "@mui/material/AppBar";
-import Toolbar from "@mui/material/Toolbar";
 import List from "@mui/material/List";
-import { Button } from "@mui/material";
 import ListItem from "@mui/material/ListItem";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
@@ -28,10 +24,6 @@ function Sidebar() {
   ];
   return (
     <Box sx={{ display: "flex" }}>
-      {/* <AppBar
-        position='fixed'
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      ></AppBar> */}
       <Drawer
         PaperProps={{
           sx: { width: "25%" },
