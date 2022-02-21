964ba73781f0c832d4bc46886a7ff8aecd4952af
Muna Dahir <muna.dahir@hotmail.com>
2022-02-20 19:05:14 -0500

    show the due date on each task

--- client/src/components/goals/TaskListItem.jsx
+++ client/src/components/goals/TaskListItem.jsx
@@ -1,24 +1,16 @@
-import Box from "@mui/material/Box";
-import Modal from "@mui/material/Modal";
-import { Divider, Typography } from "@mui/material";
+import { Typography, Chip } from "@mui/material";
+import TodayIcon from "@mui/icons-material/Today";
+import moment from "moment";
 import "./home.css";
 
-const style = {
-  position: "absolute",
-  top: "50%",
-  left: "50%",
-  transform: "translate(-50%, -50%)",
-  width: 400,
-  bgcolor: "background.paper",
-  border: "2px solid #000",
-  boxShadow: 24,
-  p: 4,
-};
 const TaskListItem = (props) => {
-  const { id, details, completedTask } = props;
+  const { id, details, completedTask, date, created } = props;
+  const taskDate = new Date(created);
+  console.log("CREATED ON ---->", taskDate);
   const clickHandler = () => {
     completedTask(id);
   };
+
   //parse the individual goals and return an component for each
   return (
     <>
@@ -28,7 +20,20 @@ const TaskListItem = (props) => {
           className='task-checkbox'
           onClick={clickHandler}
         />
-        <Typography>{details}</Typography>
+        <div className='task-details-holder'>
+          <Typography
+            sx={{
+              width: "80%",
+            }}
+          >
+            {details}
+          </Typography>
+          <Chip
+            label={`${moment(date).fromNow()}`}
+            variant='outlined'
+            sx={{ width: "8rem", color: "#999a9b" }}
+          />
+        </div>
       </div>
     </>
   );
