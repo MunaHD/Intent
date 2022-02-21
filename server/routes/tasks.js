4d810ba4aa7dcb16d0c46adcd40cbcdee78d5953
Muna Dahir <muna.dahir@hotmail.com>
2022-02-20 19:11:41 -0500

    rorder tasks by due date and add column date when adding a new task

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -10,7 +10,25 @@ const tasksRouter = (db) => {
     JOIN users ON tasks.user_id = users.id
     JOIN goals ON tasks.goal_id = goals.id
     WHERE users.email = $1
-    ORDER BY tasks.created DESC;`;
+    ORDER BY tasks.complete_by ASC;`;
+
+    const queryParams = [req.user.email];
+    return db
+      .query(queryString, queryParams)
+      .then((data) => {
+        res.json(data.rows);
+        console.log("DATA ROWS", data.rows);
+      })
+      .catch((err) => console.log(err));
+  });
+  // get the latest tasks for a user
+  router.get("/new", authenticateToken, function (req, res, next) {
+    const queryString = `
+    SELECT tasks.* FROM tasks
+    JOIN users ON tasks.user_id = users.id
+    WHERE users.email = $1 AND tasks.is_completed = false
+    ORDER BY tasks.complete_by ASC
+    LIMIT 5;`;
 
     const queryParams = [req.user.email];
     return db
@@ -22,12 +40,13 @@ const tasksRouter = (db) => {
       .catch((err) => console.log(err));
   });
 
+  //update the is_completed column when a person finishes a task
   router.put("/complete", authenticateToken, function (req, res, next) {
     const taskId = req.body.data;
     console.log("TASK ID", taskId);
     const queryString = `
     UPDATE tasks
-    SET iscompleted = true
+    SET is_completed = true
     WHERE id = $1
     RETURNING *;`;
 
@@ -40,20 +59,20 @@ const tasksRouter = (db) => {
       })
       .catch((err) => console.log(err));
   });
-  // add a task
 
+  // add a task to a goal for a user
   router.post("/", authenticateToken, function (req, res, next) {
     const userId = req.user.id;
     const details = req.body.taskData.details;
     const goalId = req.body.taskData.goal_id;
+    const completeBy = req.body.taskData.date;
 
-    // console.log("DATA ----POST---->", req.body.goalData);
     const queryString = `
-    INSERT INTO tasks (details, goal_id, user_id)
-    VALUES ($1, $2, $3)
+    INSERT INTO tasks (details, complete_by, goal_id, user_id)
+    VALUES ($1, $2, $3, $4)
     RETURNING *;`;
 
-    const queryParams = [details, goalId, userId];
+    const queryParams = [details, completeBy, goalId, userId];
     return db
       .query(queryString, queryParams)
       .then((data) => {
