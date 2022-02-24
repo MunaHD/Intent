c92545db5005253a5fe9fdcbb46be067d744bf43
Muna Dahir <muna.dahir@hotmail.com>
2022-02-23 23:23:18 -0500

    change the name of 'choice'

--- server/db/seeds/03_journals.sql
+++ server/db/seeds/03_journals.sql
@@ -1,13 +1,13 @@
 
 INSERT INTO journals (entry,choice, goal_id, user_id)
 VALUES
-('Today, was great.', 'anxiety.png', 1, 1),
-('I found it hard to concentrate.', 'body_ache.png', 1, 1),
+('Today, was great.', 'anxious.png', 1, 1),
+('I found it hard to concentrate.', 'aching.png', 1, 1),
 ('It was fun.', 'embarrassed.png', 2, 1),
 ('Still trying', 'exhausted.png', 3, 1),
-('Today, was great.', 'injury.png', 4, 1),
+('Today, was great.', 'injured.png', 4, 1),
 ('I found it hard to concentrate.', 'sick.png', 1, 2),
-('It was fun.', 'injury.png', 5, 1),
+('It was fun.', 'injured.png', 5, 1),
 ('Still trying', 'sick.png', 7, 2);
 
 
